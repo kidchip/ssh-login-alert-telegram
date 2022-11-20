833c8642823c1640bc4ea9ee09993458d9b43c68
Javier <bglez.javi@gmail.com>
2022-11-20 11:21:20 +0100

    Customize alert

--- alert.sh
+++ alert.sh
@@ -9,16 +9,13 @@ DATE="$(date "+%d %b %Y %H:%M")"
 
 if [ -n "$SSH_CLIENT" ]; then
 	CLIENT_IP=$(echo $SSH_CLIENT | awk '{print $1}')
-
 	SRV_HOSTNAME=$(hostname -f)
 	SRV_IP=$(hostname -I | awk '{print $1}')
-
-	IPINFO="https://ipinfo.io/${CLIENT_IP}"
-
-	TEXT="Connection from *${CLIENT_IP}* as ${USER} on *${SRV_HOSTNAME}* (*${SRV_IP}*)
-	Date: ${DATE}
-	More informations: [${IPINFO}](${IPINFO})"
-
+	TEXT="Someone logged in via SSH
+	Host: *${SRV_HOSTNAME}* (*${SRV_IP}*)
+	User: ${USER}
+	From: *${CLIENT_IP}*
+	Date: ${DATE}"
 	curl -s -d "chat_id=$i&text=${TEXT}&disable_web_page_preview=true&parse_mode=markdown" $URL > /dev/null
 fi
 done
